9649029b9a0ac965618d1440855ea8c616303369
pipicaso-art <pipicaso@gmail.com>
2026-06-26 14:53:05 +0900

    fix: simplify next.config.js for Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-const { withNextOnPages } = require('@cloudflare/next-on-pages/next-dev');
-
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = process.env.CF_PAGES ? withNextOnPages(nextConfig) : nextConfig;
+module.exports = nextConfig;
